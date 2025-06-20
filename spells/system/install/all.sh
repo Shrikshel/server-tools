89db77f03194a36742519f598b91694b4df9233c
Shrikshel <dev@shriko.in>
2025-06-20 17:38:53 +0530

    Fix Install And Uninstall All

--- spells/system/install/all.sh
+++ spells/system/install/all.sh
@@ -1,3 +1,5 @@
+log_info_box "Installing all system utilities..."
+
 st system install vnstat
 st system install docker
 st system install btop
@@ -9,3 +11,8 @@ st system install bat
 st system install resticprofile
 st system install rclone
 st system install restic
+
+log_info_box "All system utilities have been installed successfully. You can now use them as needed."
+
+log_info "Below is the list of installed utilities:"
+log_info "vnstat, docker, btop, eza, ncdu, duf, ufw, bat, resticprofile, rclone, restic"
--- spells/system/uninstall/all.sh
+++ spells/system/uninstall/all.sh
@@ -1,3 +1,5 @@
+log_info_box "Uninstalling all system utilities..."
+
 st system uninstall vnstat
 st system uninstall docker
 st system uninstall btop
@@ -9,3 +11,7 @@ st system uninstall bat
 st system uninstall resticprofile
 st system uninstall rclone
 st system uninstall restic
+
+log_info_box "All system utilities have been uninstalled successfully. You can now reinstall them as needed."
+log_info "Below is the list of uninstalled utilities:"
+log_info "vnstat, docker, btop, eza, ncdu, duf, ufw, bat, resticprofile, rclone, restic"
