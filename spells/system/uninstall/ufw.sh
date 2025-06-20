0dd4e318681f258efb47c2d1167c9069bd69e059
Shrikshel <dev@shriko.in>
2025-06-20 17:33:33 +0530

    Fix UFW Uninstall

--- spells/system/uninstall/ufw.sh
+++ spells/system/uninstall/ufw.sh
@@ -3,9 +3,9 @@
 if command -v ufw &>/dev/null; then
     log_info "🔧 Uninstalling ufw..."
     sudo apt-get remove --purge -y ufw
+    log_info "ufw Uninstalled Successfully"
 else
-    log_success "✅ ufw is not installed."
+    log_info "ufw is not installed."
 fi
 
-log_success "ufw Uninstalled Successfully"
 log_info_box "To reinstall ufw, run 'st system install ufw'."
