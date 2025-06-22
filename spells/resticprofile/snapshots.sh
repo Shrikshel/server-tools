1575a792b6905a6d34432663229197fabbbb271e
Shrikshel <dev@shriko.in>
2025-06-22 10:12:44 +0530

    Add Spell rp snapshots

---
+++ spells/resticprofile/snapshots.sh
@@ -0,0 +1,5 @@
+echo "# This file is located at 'src/../spells/resticprofile/snapshots.sh'."
+echo "# It contains the implementation for the 'st resticprofile snapshots' command."
+echo "# The code you write here will be wrapped by a function named 'st_resticprofile_snapshots_command()'."
+echo "# Feel free to edit this file; your changes will persist when regenerating."
+inspect_args
